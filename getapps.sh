cf00ef886dcf70400b5fb4dfe60a39d044c2c1ca
Andreas Petzel <apetzel@APEcore-Pro.local>
2019-07-17 10:52:44 +0200

    update software list comment out not used apps anymore

--- getapps.sh
+++ getapps.sh
@@ -4,11 +4,11 @@ mkdir ~/TMP_APPS
 cd ~/TMP_APPS
 
 # Installing Chrome
-curl -L -O "https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg"
-hdiutil mount -nobrowse googlechrome.dmg
-cp -R "/Volumes/Google Chrome/Google Chrome.app" /Applications
-hdiutil unmount "/Volumes/Google Chrome"
-rm googlechrome.dmg
+#curl -L -O "https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg"
+#hdiutil mount -nobrowse googlechrome.dmg
+#cp -R "/Volumes/Google Chrome/Google Chrome.app" /Applications
+#hdiutil unmount "/Volumes/Google Chrome"
+#rm googlechrome.dmg
 
 # Installing Firefox
 curl -L -o Firefox.dmg "http://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US"
@@ -18,11 +18,11 @@ hdiutil unmount "/Volumes/Firefox"
 rm Firefox.dmg
 
 # Installing Handbrake
-curl -L -o HandBrake.dmg "http://handbrake.fr/rotation.php?file=HandBrake-0.10.5-MacOSX.6_GUI_x86_64.dmg"
-hdiutil mount -nobrowse HandBrake.dmg -mountpoint /Volumes/HandBrake
-cp -R "/Volumes/HandBrake/HandBrake.app" /Applications
-hdiutil unmount "/Volumes/HandBrake"
-rm HandBrake.dmg
+#curl -L -o HandBrake.dmg "http://handbrake.fr/rotation.php?file=HandBrake-0.10.5-MacOSX.6_GUI_x86_64.dmg"
+#hdiutil mount -nobrowse HandBrake.dmg -mountpoint /Volumes/HandBrake
+#cp -R "/Volumes/HandBrake/HandBrake.app" /Applications
+#hdiutil unmount "/Volumes/HandBrake"
+#rm HandBrake.dmg
 
 # Installing Spotify
 curl -L -O "http://download.spotify.com/Spotify.dmg"
@@ -32,45 +32,51 @@ hdiutil unmount "/Volumes/Spotify"
 rm Spotify.dmg
 
 # Installing VLC Player
-curl -L -o vlc.dmg "http://get.videolan.org/vlc/2.2.2/macosx/vlc-2.2.2.dmg"
-hdiutil mount -nobrowse vlc.dmg -mountpoint /Volumes/vlc
-cp -R "/Volumes/vlc/VLC.app" /Applications
-hdiutil unmount "/Volumes/vlc"
-rm vlc.dmg
+#curl -L -o vlc.dmg "http://get.videolan.org/vlc/2.2.2/macosx/vlc-2.2.2.dmg"
+#hdiutil mount -nobrowse vlc.dmg -mountpoint /Volumes/vlc
+#cp -R "/Volumes/vlc/VLC.app" /Applications
+#hdiutil unmount "/Volumes/vlc"
+#rm vlc.dmg
 
 # Installing Transmission
-curl -L -o Transmission.dmg "https://transmission.cachefly.net/Transmission-2.84.dmg"
-hdiutil mount -nobrowse Transmission.dmg
-cp -R "/Volumes/Transmission/Transmission.app" /Applications
-hdiutil unmount "/Volumes/Transmission"
-rm Transmission.dmg
+#curl -L -o Transmission.dmg "https://transmission.cachefly.net/Transmission-2.84.dmg"
+#hdiutil mount -nobrowse Transmission.dmg
+#cp -R "/Volumes/Transmission/Transmission.app" /Applications
+#hdiutil unmount "/Volumes/Transmission"
+#rm Transmission.dmg
 
 # Installing Skype
-curl -L -O "http://www.skype.com/go/getskype-macosx.dmg"
-hdiutil mount -nobrowse getskype-macosx.dmg
-cp -R "/Volumes/Skype/Skype.app" /Applications
-hdiutil unmount "/Volumes/Skype"
-rm getskype-macosx.dmg
+#curl -L -O "http://www.skype.com/go/getskype-macosx.dmg"
+#hdiutil mount -nobrowse getskype-macosx.dmg
+#cp -R "/Volumes/Skype/Skype.app" /Applications
+#hdiutil unmount "/Volumes/Skype"
+#rm getskype-macosx.dmg
 
 # Installing Dropbox
-curl -L -o Dropbox.dmg "https://www.dropbox.com/download?plat=mac"
-hdiutil mount -nobrowse Dropbox.dmg
-cp -R "/Volumes/Dropbox Installer/Dropbox.app" /Applications
-hdiutil unmount "/Volumes/Dropbox Installer"
-rm Dropbox.dmg
+#curl -L -o Dropbox.dmg "https://www.dropbox.com/download?plat=mac"
+#hdiutil mount -nobrowse Dropbox.dmg
+#cp -R "/Volumes/Dropbox Installer/Dropbox.app" /Applications
+#hdiutil unmount "/Volumes/Dropbox Installer"
+#rm Dropbox.dmg
 
 # Installing Google Drive
-curl -L -O "https://dl-ssl.google.com/drive/installgoogledrive.dmg"
-hdiutil mount -nobrowse installgoogledrive.dmg
-cp -R "/Volumes/Install Google Drive/Google Drive.app" /Applications
-hdiutil unmount "/Volumes/Install Google Drive"
-rm installgoogledrive.dmg
+#curl -L -O "https://dl-ssl.google.com/drive/installgoogledrive.dmg"
+#hdiutil mount -nobrowse installgoogledrive.dmg
+#cp -R "/Volumes/Install Google Drive/Google Drive.app" /Applications
+#hdiutil unmount "/Volumes/Install Google Drive"
+#rm installgoogledrive.dmg
 
 # Installing caffeine
 curl -L -o Caffeine.zip "http://download.lightheadsw.com/download.php?software=caffeine"
 unzip Caffeine.zip
 mv Caffeine.app /Applications
 rm Caffeine.zip
+
+# Installing Atom
+curl -L -o atom.zip "https://atom.io/download/mac/atom-mac.zip"
+unzip atom.zip
+mv Atom.app /Applications
+rm atom.zip
 
 # Installing Sublime Text 3 Beta
 curl -L -o "Sublime Text 3.dmg" "https://download.sublimetext.com/Sublime%20Text%20Build%203103.dmg"
@@ -93,11 +99,11 @@ hdiutil unmount "/Volumes/Sequel Pro 1.1.0.1"
 rm sequel-pro.dmg
 
 # Installing Source Tree
-curl -L -o SourceTree.dmg "http://downloads.atlassian.com/software/sourcetree/SourceTree_2.2.dmg"
-hdiutil mount -nobrowse SourceTree.dmg
-cp -R "/Volumes/SourceTree/SourceTree.app" /Applications
-hdiutil unmount "/Volumes/SourceTree"
-rm SourceTree.dmg
+#curl -L -o SourceTree.dmg "http://downloads.atlassian.com/software/sourcetree/SourceTree_2.2.dmg"
+#hdiutil mount -nobrowse SourceTree.dmg
+#cp -R "/Volumes/SourceTree/SourceTree.app" /Applications
+#hdiutil unmount "/Volumes/SourceTree"
+#rm SourceTree.dmg
 
 # Installing FileZilla
 curl -L -o Filezilla.app.tar.bz2 "http://sourceforge.net/projects/filezilla/files/FileZilla_Client/3.15.0.2/FileZilla_3.15.0.2_macosx-x86.app.tar.bz2"
@@ -145,10 +151,16 @@ cp -R "/Volumes/KeepassX/KeePassX.app" /Applications
 hdiutil unmount "/Volumes/KeepassX"
 rm KeepassX.dmg
 
-# Installing Kodi
-curl -L -o kodi.dmg "http://mirrors.kodi.tv/releases/osx/x86_64/kodi-15.2-Isengard-x86_64.dmg"
-hdiutil mount -nobrowse kodi.dmg
-cp -R "/Volumes/Kodi/Kodi.app" /Applications
-hdiutil unmount "/Volumes/Kodi"
-rm kodi.dmg
+# Installing Keka
+curl -L -o Keka.dmg "http://download.kekaosx.com/"
+hdiutil mount -nobrowse Keka.dmg
+cp -R "/Volumes/Keka/Keka.app" /Applications
+hdiutil unmount "/Volumes/Keka"
+rm Keka.dmg
 
+# Installing Kodi
+#curl -L -o kodi.dmg "http://mirrors.kodi.tv/releases/osx/x86_64/kodi-15.2-Isengard-x86_64.dmg"
+#hdiutil mount -nobrowse kodi.dmg
+#cp -R "/Volumes/Kodi/Kodi.app" /Applications
+#hdiutil unmount "/Volumes/Kodi"
+#rm kodi.dmg
